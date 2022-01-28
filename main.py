234687e2cde66c02254f6b17150f1d3b4acbe559
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2022-01-28 11:31:54 +0100

    Add more statistics

--- main.py
+++ main.py
@@ -368,7 +368,7 @@ def extract_app_urls(app_dir):
     for root, dirs, files in os.walk(app_dir):
         for file in files:
             if os.path.splitext(file)[1] not in ['.smali', '.xml', '.js', '.html', '.java', '.json']:
-                # Do not extract URLs from text file such as README or LICENSE files
+                # Do not extract URLs from text file such as README or LICENSE files or binary files
                 continue
 
             try:
@@ -620,7 +620,7 @@ def analyze_tls(force=False, force_failed=False):
             data = json.load(f)
             for domain in data['domains']:
                 # Only analyze domains that haven't been analyzed before, unless forced otherwise
-                if domain in tls_configs and not force and not (force_failed and (domain not in tls_configs or tls_configs[domain] is None)):
+                if domain in tls_configs and not force and not (force_failed and (domain not in tls_configs or tls_configs[domain] is False)):
                     print(colored(f'Skipping analysis of TLS of {domain}, already analyzed', 'yellow'))
                     continue
                 else:
@@ -684,8 +684,19 @@ def calculate_statistics():
     http_only_apps  = 0  # Number of apps that only use HTTP requests
     https_only_apps = 0  # Number of apps that only use HTTPS requests
 
+    http_only_top_apps = 0  # Number of top apps that only use HTTP requests
+    https_only_top_apps = 0  # Number of top apps that only use HTTPS requests
+
+    http_only_bottom_apps = 0  # Number of bottom apps that only use HTTP requests
+    https_only_bottom_apps = 0  # Number of bottom apps that only use HTTPS requests
+
     http_domains  = set()  # Set of domains using HTTP in some application
     https_domains = set()  # Set of domains using HTTPS in some application
+
+    # Get top and bottom apps
+    top_bottom_apps_count = 50
+    top_apps = get_top_apps(top_bottom_apps_count)
+    bottom_apps = get_bottom_apps(top_bottom_apps_count)
 
     # Loop through JSON files of applications containing URLs, and gather statistics
     for urls_json_file in urls_json_files:
@@ -693,11 +704,21 @@ def calculate_statistics():
         with open(urls_json_file, 'r') as f:
             data = json.load(f)
 
+        package_id = path.basename(path.dirname(urls_json_file))
+
         # HTTP / HTTPS usage of apps
         if data['url_https_count'] == 0 and data['url_http_count'] != 0:
             http_only_apps += 1
+            if package_id in top_apps:
+                http_only_top_apps += 1
+            if package_id in bottom_apps:
+                http_only_bottom_apps += 1
         elif data['url_https_count'] != 0 and data['url_http_count'] == 0:
             https_only_apps += 1
+            if package_id in top_apps:
+                https_only_top_apps += 1
+            if package_id in bottom_apps:
+                https_only_bottom_apps += 1
 
         # HTTP / HTTPS usage for domains
         for url in data['urls']:
@@ -765,16 +786,16 @@ def calculate_statistics():
             else:
                 domains_certificate_issues[domain_tls['verifyCertError']] += 1
 
-        tls_support = [tls_version for tls_version in domain_tls['tlsVersions'] if tls_version[:len('TLS')] == 'TLS']
-        if domain_tls['tlsVersion'].replace('.', '_') not in tls_support:
-            # Sometimes, the TLS version in the tlsVersion field is not listed in the tlsVersions field.
-            tls_support.append(domain_tls['tlsVersion'].replace('.', '_'))
-        tls_support.sort()
-        tls_support = ', '.join([tls[len('TLSv'):].replace('_', '.') for tls in tls_support])
-        if tls_support not in domains_tls_configs:
-            domains_tls_configs[tls_support] = 1
-        else:
-            domains_tls_configs[tls_support] += 1
+        tls_support = [tls_version[len('TLSv'):].replace('_', '.')
+                       for tls_version in domain_tls['tlsVersions']
+                       if tls_version[:len('TLS')] == 'TLS']
+        for tls_version in tls_support:
+            if tls_version not in domains_tls_configs:
+                domains_tls_configs[tls_version] = 1
+            else:
+                domains_tls_configs[tls_version] += 1
+
+    (can_use_cleartext, cannot_use_cleartext) = calculate_cleartext_traffic_usage()
 
     # Display statistics for HTTP/HTTPS usage in apps
     labels = ('HTTP only', 'Mixed', 'HTTPS only')
@@ -782,26 +803,62 @@ def calculate_statistics():
     values = [http_only_apps, application_count - http_only_apps - https_only_apps, https_only_apps]
     plot_bar_chart(labels, values, 'Apps', 'HTTP/HTTPS usage in apps')
 
+    labels = ('HTTP only', 'Mixed', 'HTTPS only')
+    values = [http_only_top_apps, top_bottom_apps_count - http_only_top_apps - https_only_top_apps, https_only_top_apps]
+    plot_bar_chart(labels, values, 'Apps', f'HTTP/HTTPS usage in top {top_bottom_apps_count} apps')
+
+    labels = ('HTTP only', 'Mixed', 'HTTPS only')
+    values = [http_only_bottom_apps, top_bottom_apps_count - http_only_bottom_apps - https_only_bottom_apps, https_only_bottom_apps]
+    plot_bar_chart(labels, values, 'Apps', f'HTTP/HTTPS usage in bottom {top_bottom_apps_count} apps')
+
     # Display statistics for HTTP/HTTPS usage for domains
     labels = ('HTTP only', 'Mixed', 'HTTPS only')
     values = [len(http_only_domains), len(mixed_domains), len(https_only_domains)]
     plot_bar_chart(labels, values, 'Domains', 'HTTP/HTTPS usage for domains')
 
     # Display statistics for TLS configuration of domains
-    labels = ('Unresolved', 'No TLS HTTP', 'TLS HTTP', 'No TLS Mixed', 'TLS Mixed', 'No TLS HTTPS', 'TLS HTTPS')
+    labels = ('Unresolved', 'No TLS, using HTTP', 'TLS, using HTTP', 'No TLS, using Mixed', 'TLS, using Mixed', 'No TLS, using HTTPS', 'TLS, using HTTPS')
     values = [unresolved_domains, no_tls_http_domains, tls_http_domains, no_tls_mixed_domains, tls_mixed_domains, no_tls_https_domains, tls_https_domains]
     plot_bar_chart(labels, values, 'Domains', 'TLS configuration for domains')
 
     # Display statistics for TLS certificate errors
     labels = list(domains_certificate_issues.keys())
     values = list(domains_certificate_issues.values())
-    plot_bar_chart(labels, values, 'Domains', 'TLS certificate errors', True)
+    plot_bar_chart(labels, values, 'Domains', 'TLS certificate errors of HTTPS domains', True)
 
     # Display statistics for TLS versions
-    domains_tls_configs = {k: v for k, v in domains_tls_configs.items() if v > 100}  # Do not show configurations with negligible usage
     labels = list(domains_tls_configs.keys())
     values = list(domains_tls_configs.values())
-    plot_bar_chart(labels, values, 'Domains', 'TLS versions', True)
+    plot_bar_chart(labels, values, 'Domains', 'TLS versions supported by domains')
+
+    # Display statistics for cleartext traffic usage
+    labels = ('Cleartext traffic', 'No cleartext traffic')
+    values = [can_use_cleartext, cannot_use_cleartext]
+    plot_bar_chart(labels, values, 'Apps', 'Usage of usesCleartextTraffic flag')
+
+
+def calculate_cleartext_traffic_usage():
+    # Enumerate AndroidManifest.xml files of applications
+    manifest_files = [file for file in glob.glob(path.join(workdir, 'decompiled', '*', 'AndroidManifest.xml')) if path.isfile(file)]
+
+    # Statistics
+    can_use_cleartext = 0
+    cannot_use_cleartext = 0
+
+    for manifest_file in manifest_files:
+        with open(manifest_file, 'r') as f:
+            manifest_xml = f.read()
+
+        # Extract SDK version using regex
+        sdk_version = re.search('platformBuildVersionCode="(.*?)"', manifest_xml).group(1)
+        if sdk_version >= '28':
+            # App should include usesCleartextTraffic to use HTTP traffic
+            if 'android:usesCleartextTraffic="true"' in manifest_xml:
+                can_use_cleartext += 1
+            else:
+                cannot_use_cleartext += 1
+
+    return can_use_cleartext, cannot_use_cleartext
 
 
 def add_bar_chart_labels(labels, values):
@@ -816,32 +873,66 @@ def plot_bar_chart(labels, values, y_label, title, rotate_labels=False):
     plt.bar(y_pos, values, align='center')
     add_bar_chart_labels(labels, values)
     if rotate_labels:
-        plt.xticks(y_pos, [label.capitalize() for label in labels], rotation=-10, ha='left')
+        plt.xticks(y_pos, map(uc_first, labels), rotation=-10, ha='left')
     else:
-        plt.xticks(y_pos, [label.capitalize() for label in labels])
+        plt.xticks(y_pos, map(uc_first, labels))
     plt.ylabel(y_label)
     plt.title(title)
     fig.tight_layout()
     plt.show()
 
 
-def mozilla_tls_configuration_security(configuration):
-    # The TLS version reported in tlsVersion does not always seem to be in the tlsVersions list
-    if configuration['tlsVersion'] not in configuration['tlsVersions']:
-        configuration['tlsVersions'].append(configuration['tlsVersion'].replace('.', '_'))
+def get_top_apps(amount=100):
+    if not path.exists(path.join(workdir, 'apps.json')):
+        print(colored('No apps.json file found, cannot determine top apps', 'yellow'))
+        return None
 
-    # TODO Add cipher checks
-    if 'TLSv1_3' in configuration['tlsVersions']:
-        if len(configuration['tlsVersions']) == 1:
-            # Supports only TLS 1.3
-            return 'modern'
-        elif 'TLSv1_2' in configuration['tlsVersions'] and \
-                len(configuration['tlsVersions']) == 2:
-            # Supports only TLS 1.2 and TLS 1.3
-            return 'intermediate'
-        else:
-            return 'old'
+    # Loop through apps to download
+    with open(path.join(workdir, 'apps.json')) as f:
+        packages = json.load(f)
+
+    top_apps = []
+    index = 1
+    while len(top_apps) < amount:
+        for package in packages:
+            if package['index'] == index:
+                top_apps.append(package['package_id'])
+            if len(top_apps) == amount:
+                break
+        index += 1
+
+    return top_apps
+
+
+def get_bottom_apps(amount=100):
+    if not path.exists(path.join(workdir, 'apps.json')):
+        print(colored('No apps.json file found, cannot determine bottom apps', 'yellow'))
+        return None
+
+    # Loop through apps to download
+    with open(path.join(workdir, 'apps.json')) as f:
+        packages = json.load(f)
+
+    bottom_apps = []
+    index = max(package['index'] for package in packages)
+    while len(bottom_apps) < amount:
+        for package in packages:
+            if package['index'] == index:
+                bottom_apps.append(package['package_id'])
+            if len(bottom_apps) == amount:
+                break
+        index -= 1
+
+    return bottom_apps
+
+
+
+def uc_first(string):
+    return string[0].upper() + string[1:]
 
 
 if __name__ == '__main__':
     run()
+
+# TODO
+# - Add graphs comparing e.g. top 100 to bottom 100
